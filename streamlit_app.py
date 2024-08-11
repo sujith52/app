021b420e3f05967d0e26309d7a0209bf770328eb
sujith kumar <148097955+sujith52@users.noreply.github.com>
2024-08-11 19:27:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,61 +3,47 @@ import pandas as pd
 
 st.title('Machine Learning App')
 
-st.info('thi sis a machine learning app, built by machie learning model')
+st.info('This is a machine learning app, built by a machine learning model')
 
-with st.expander('Data'):
-  st.write('**Raw Data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-  df
+# Load the penguins dataset
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
-  st.write('**X**')
-  x= df.drop('species',axis=1)
-  x
+# Display raw data
+st.write('**Raw Data**')
+st.write(df)
 
-  st.write('**Y**')
-  y=df.species
-  y
-#"bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+# Data preparation
 with st.expander('Data Visualization'):
-  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+    st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
-#data preparations
+# Sidebar input features
 with st.sidebar:
-  st.header('Input Features')
-  island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
-  gender = st.selectbox('Gender',('Male','Female'))
-  bill_length_mm = st.slider('Bill Length (mm)',32.1,59.6,43.9)
-  bill_depth_mm = st.slider('Bill Depth (mm)',13.1,21.5,17.2)
-  flipper_length = st.slider('Flipper Length (mm)', 172.0,231.0,201.0)
-  body_mass_g  = st.slider('Body Mass (g)',2700.0,6300.0,4207.0)
-  gender = st.selectbox('Gender',('male','female'))
+    st.header('Input Features')
+    island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+    gender = st.selectbox('Gender', ('Male', 'Female'))
+    bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
+    bill_depth_mm = st.slider('Bill Depth (mm)', 13.1, 21.5, 17.2)
+    flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)  # Corrected variable name
+    body_mass_g = st.slider('Body Mass (g)', 2700.0, 6300.0, 4207.0)
 
-  #craetimg the data frame ffor the input features
-  data = {'island', island,
-          'bill_length_mm':bill_length_mm,
-          'bill_depth_mm': bill_depth_mm,
-          'flipper_length_mm': flipper_length_mm,
-          'body_mass_g': body_mass_g,
-          'gender': gender}
-  input_df  = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
+    # Create the input DataFrame
+    data = {
+        'island': island,
+        'bill_length_mm': bill_length_mm,
+        'bill_depth_mm': bill_depth_mm,
+        'flipper_length_mm': flipper_length_mm,
+        'body_mass_g': body_mass_g,
+        'gender': gender
+    }
+    input_df = pd.DataFrame(data, index=[0])
 
+# Concatenate input features with existing data
+input_penguins = pd.concat([input_df, df.drop('species', axis=1)], axis=0)
+
+# Display input features DataFrame
 with st.expander('Input Features'):
-  st.write(**Input Penguin**)
-input_df
-st.write('**Conbined Penguins Data**')
-input_penguins
+    st.write(input_df)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Display combined penguins data
+st.write('**Combined Penguins Data**')
+st.write(input_penguins)
