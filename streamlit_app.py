823f9a6c4398789604d910bbbe469cbaaa41a0bc
sujith kumar <148097955+sujith52@users.noreply.github.com>
2024-08-11 12:57:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ st.title('Machine Learning App')
 
 st.info('thi sis a machine learning app, built by machie learning model')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
