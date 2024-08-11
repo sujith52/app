503c49dd909a2ad9689fb5bb0099b3911be46402
sujith kumar <148097955+sujith52@users.noreply.github.com>
2024-08-11 12:44:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Machine Learning App')
 
-st.write('Hello world!')
+st.info('thi sis a machine learning app, built by machie learning model')
